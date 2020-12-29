7cf1e83fd2336aa0e92cbd7f970f9e6aa11fdf1c
Rameshaaa <rammech465@gmail.com>
2020-12-29 18:41:04 +0530

    First Commit

---
+++ src/test/java/Azure/Devops/AzureTest.java
@@ -0,0 +1,18 @@
+package Azure.Devops;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AzureTest {
+
+	@Test
+	public void azuretest() {
+
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\561877\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://webapps.tekstac.com/CustomerRegistration/");
+	}
+
+}
