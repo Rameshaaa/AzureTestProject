1dfce05bb89ce0d5b9d41de0d0deb9320017a772
Rameshaaa <68278771+Rameshaaa@users.noreply.github.com>
2020-12-29 19:00:04 +0530

    Update AzureTest.java

--- src/test/java/Azure/Devops/AzureTest.java
+++ src/test/java/Azure/Devops/AzureTest.java
@@ -13,6 +13,7 @@ public class AzureTest {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\561877\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://webapps.tekstac.com/CustomerRegistration/");
+		System.out.println("URL launched");
 	}
 
 }
